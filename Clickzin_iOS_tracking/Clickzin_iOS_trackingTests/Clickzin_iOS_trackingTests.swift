a125cf2ad3656e0d08c404f4ac27ec2c15aa9085
skingithub <sksenthilkumar@live.com>
2025-04-14 18:06:49 +0530

    Initial project

---
+++ Clickzin_iOS_tracking/Clickzin_iOS_trackingTests/Clickzin_iOS_trackingTests.swift
@@ -0,0 +1,11 @@
+
+import Testing
+@testable import Clickzin_iOS_tracking
+
+struct Clickzin_iOS_trackingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
