7feada2fc770e3adbae1b184ac3613036ff7ec57
skingithub <sksenthilkumar@live.com>
2025-04-16 07:45:30 +0530

    Included empty functions

--- Clickzin_iOS_tracking/Clickzin_iOS_tracking/Clickzin_iOS_tracking.swift
+++ Clickzin_iOS_tracking/Clickzin_iOS_tracking/Clickzin_iOS_tracking.swift
@@ -1,3 +1,13 @@
 
 import Foundation
 
+public class ClickzinTracking {
+    
+    public static func startTracking(){
+        print("Tracking intiated")
+    }
+    
+    public static func trackEvent(eventName: String){
+        print("Event \(eventName) tracked")
+    }
+}
